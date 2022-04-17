fe395036c7aea31f4e8306af7a7cd655d5fdc00c
AlexandreDevaux <alexandre.devaux@epita.fr>
2022-04-17 18:58:22 +0200

    v1.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import * as gtag from '../lib/gtag'
 
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { hotjar } from 'react-hotjar'
+import TagManager from 'react-gtm-module'
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url: any)  => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   useEffect(() => {
     hotjar.initialize(process.env.HJID, process.env.HJSV)
   }, [])
+  useEffect(() => {
+
+    if (typeof window !== 'undefined') {
+      console.log("init GTM")
+      TagManager.initialize({ gtmId: process.env.GID });
+    }else {
+      console.log("GTM server side - ignorning")
+    }
+
+}, [])
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,30 +12,13 @@ import Script from 'next/script'
 
 
 const Home: NextPage = () => {
-  const GID : string = process.env.GID
-  const url : string = `https://www.googletagmanager.com/gtag/js?id=${GID}`
-
   return (
     <div>
       <Head>
         <title>Overa Landing Page</title>
         <meta name="Overa Landing Page" content="This page is the landing page of Overa" />
         <link rel="icon" href="/favicon.ico" />
-        
       </Head>
-      <Script
-        src={url}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${GID});
-        `}
-      </Script>
       
       <div className={styles.container}>
         <Header/>
