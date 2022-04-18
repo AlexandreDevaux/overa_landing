402ae0205dd8dd2e43eebca4b404eb5490ff6069
AlexandreDevaux <alexandre.devaux@epita.fr>
2022-04-18 17:27:41 +0200

    connected to google sheets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   env:{
@@ -6,7 +7,10 @@ const nextConfig = {
     HJSV: process.env.HJSV,
     GADS: process.env.GADS,
     GTM: process.env.GTM,
-    GA: process.env.GA
+    GA: process.env.GA,
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    GSHEET_ID: process.env.GSHEET_ID,
   }
 }
 
--- pages/api/waitlist.ts
+++ pages/api/waitlist.ts
@@ -1,8 +1,9 @@
-// To make sure only valid emails are sent to us, install email validator:
-//  $ yarn add email-validator 
-//  $ # or
-//  $ npm i --save email-validator
 import validator from "email-validator"
+import { google } from 'googleapis';
+
+
+
+
 
 export default async function handler(req: { method: string; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) {
     // We only want to handle POST requests, everything else gets a 404
@@ -23,6 +24,27 @@ async function postHandler(req: { method?: string; body?: any; }, res: { status:
 async function saveEmail(email: string) {
     // TODO: what to do here?
     console.log("Got email: " + email)
+
+    console.log(process.env.GOOGLE_PRIVATE_KEY)
+    const auth = new google.auth.GoogleAuth({
+        credentials: {
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        },
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
+    const response = await sheets.spreadsheets.values.append({
+        spreadsheetId: process.env.GSHEET_ID,
+        range: 'Sheet1!A:A',
+        valueInputOption: 'USER_ENTERED',
+        requestBody: {
+            values: [
+                [email],
+            ]
+        }
+    });
 }
 
 // Make sure we receive a valid email
